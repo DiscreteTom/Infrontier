345ee4aafe2e804ecc3366d7f835ba405c36a9e3
DiscreteTom <discrete_tom@outlook.com>
2021-12-13 16:59:51 +0800

    better empty folder tip

--- src/renderer/components/DirentList.vue
+++ src/renderer/components/DirentList.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
     <v-list nav dense>
-      <v-list-item
-        v-for="path in pathNotEmpty"
-        :key="path"
-        :to="$route.path + path"
-      >
-        <v-list-item-icon>
-          <v-icon>{{
-            path.endsWith("/") ? "mdi-folder-outline" : "mdi-file-outline"
-          }}</v-icon>
-        </v-list-item-icon>
+      <template v-if="pathNotEmpty.length">
+        <v-list-item
+          v-for="path in pathNotEmpty"
+          :key="path"
+          :to="$route.path + path"
+        >
+          <v-list-item-icon>
+            <v-icon>{{
+              path.endsWith("/") ? "mdi-folder-outline" : "mdi-file-outline"
+            }}</v-icon>
+          </v-list-item-icon>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ path }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>{{ path }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+      <template v-else>
+        <v-alert color="blue-grey" type="info" dense>
+          No objects in this folder.
+        </v-alert>
+      </template>
     </v-list>
   </div>
 </template>
