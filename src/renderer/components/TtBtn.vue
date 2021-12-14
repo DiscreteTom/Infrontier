5d40f8e91062cf15e28ec1114cfd532db02afd8f
DiscreteTom <discrete_tom@outlook.com>
2021-12-14 10:46:03 +0800

    add TtBtn

---
+++ src/renderer/components/TtBtn.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-tooltip :top="top" :bottom="bottom" :left="left" :right="right">
+    <template v-slot:activator="{ on }">
+      <v-btn
+        :icon="icon.length != 0"
+        v-on="on"
+        @click.stop="$emit('click')"
+        :to="to"
+      >
+        <v-icon>{{ icon }}</v-icon>
+        {{ btnText }}
+      </v-btn>
+    </template>
+    <span>{{ tt }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+    tt: String,
+    btnText: String,
+    top: Boolean,
+    bottom: Boolean,
+    left: Boolean,
+    right: Boolean,
+    to: String,
+  },
+};
+</script>
