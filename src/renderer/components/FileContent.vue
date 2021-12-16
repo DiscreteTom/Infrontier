f157912fe2869d40e2dc06ae0f00d0990bdf4d5f
DiscreteTom <discrete_tom@outlook.com>
2021-12-16 16:46:25 +0800

    add csv preview

--- src/renderer/components/FileContent.vue
+++ src/renderer/components/FileContent.vue
@@ -1,30 +1,89 @@
 <template>
   <div>
-    <img
-      v-if="['jpg', 'jpeg', 'png'].indexOf(fileType) != -1"
-      :src="imgSrc()"
-    />
-    <pre v-else>{{ content.toString("utf-8") }}</pre>
+    <div v-if="loading" class="d-flex justify-center">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+        :size="50"
+        class="my-3"
+      ></v-progress-circular>
+    </div>
+    <div v-else>
+      <img
+        v-if="['jpg', 'jpeg', 'png'].indexOf(fileType) != -1"
+        :src="imgSrc"
+      />
+      <v-simple-table v-else-if="fileType == 'csv'">
+        <template v-slot:default>
+          <tbody>
+            <tr v-for="(line, index) in csvContent" :key="index">
+              <td v-for="column in line" :key="column">{{ column }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <pre v-else>{{ content.toString("utf-8") }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
+import * as csv from "csv";
+
+const streamToString = (stream) =>
+  new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf-8")));
+  });
+
 export default {
   props: {
     content: Buffer,
   },
+  data() {
+    return {
+      loading: false,
+      fileType: "",
+      imgSrc: "",
+      csvContent: null,
+    };
+  },
   methods: {
-    imgSrc() {
+    parseImg() {
       let type = "";
       if (["jpg", "jpeg"].indexOf(this.fileType) != -1) type = "jpeg";
       else if (this.fileType == "png") type = "png";
-      return `data:image/${type};base64,` + this.content.toString("base64");
+      else {
+        this.imgSrc = "";
+        return;
+      }
+      this.imgSrc =
+        `data:image/${type};base64,` + this.content.toString("base64");
+    },
+    parseCsv() {
+      if (this.fileType == "csv") {
+        this.loading = true;
+        csv.parse(
+          this.content,
+          {
+            delimiter: ",",
+          },
+          (err, data) => {
+            this.csvContent = data;
+            this.loading = false;
+          }
+        );
+      } else {
+        this.csvContent = null;
+      }
     },
   },
-  computed: {
-    fileType() {
-      return this.$route.path.split("/").at(-1).split(".").at(-1);
-    },
+  mounted() {
+    this.fileType = this.$route.path.split("/").at(-1).split(".").at(-1);
+    this.parseImg();
+    this.parseCsv();
   },
 };
 </script>
