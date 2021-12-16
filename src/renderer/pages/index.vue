72e11e6227d47f5cc50ffc1ec4211415a7a67657
DiscreteTom <discrete_tom@outlook.com>
2021-12-16 11:22:56 +0800

    optimize index page

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h2>👈 Select a folder to start</h2>
+    <div v-if="$store.state.bucketName">
+      <h1>👈 Select a folder to start</h1>
+      <p class="mx-3 my-3">Current bucket: {{ $store.state.bucketName }}</p>
+    </div>
+    <div v-else>
+      <h2>
+        You need to click 'Settings' to configure this application first ↗
+      </h2>
+    </div>
   </div>
 </template>
 
