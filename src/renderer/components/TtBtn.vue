a568489110766b214d8266731bfd8b5b561294ba
DiscreteTom <discrete_tom@outlook.com>
2021-12-20 14:45:29 +0800

    show tooltip on disabled btn

--- src/renderer/components/TtBtn.vue
+++ src/renderer/components/TtBtn.vue
@@ -1,18 +1,19 @@
 <template>
   <v-tooltip :top="top" :bottom="bottom" :left="left" :right="right">
     <template v-slot:activator="{ on }">
-      <v-btn
-        :icon="icon.length != 0"
-        v-on="on"
-        @click.stop.prevent="$emit('click')"
-        :to="to"
-        :disabled="disabled"
-      >
-        <v-badge dot :value="badge">
-          <v-icon>{{ icon }}</v-icon>
-        </v-badge>
-        {{ btnText }}
-      </v-btn>
+      <div v-on="on">
+        <v-btn
+          :icon="icon.length != 0"
+          @click.stop.prevent="$emit('click')"
+          :to="to"
+          :disabled="disabled"
+        >
+          <v-badge dot :value="badge">
+            <v-icon>{{ icon }}</v-icon>
+          </v-badge>
+          {{ btnText }}
+        </v-btn>
+      </div>
     </template>
     <span>{{ tt }}</span>
   </v-tooltip>
